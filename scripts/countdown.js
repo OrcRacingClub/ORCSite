ed4316a80d392a5d811a5996b0840b218c52016f
ORCNFT <orcracingclubnft@gmail.com>
2021-10-10 10:18:28 +0100

    countdown -1 day

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,6 +1,6 @@
 // Set the date we're counting down to
 // month indexed from 0, confusing I know
-var countDownDate = new Date(Date.UTC(2021, 9, 11, 21, 0, 0)).getTime();
+var countDownDate = new Date(Date.UTC(2021, 9, 10, 21, 0, 0)).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
